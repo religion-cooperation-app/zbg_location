63f01bab04cb8e5156006d42a385c776dea0282c
Zach Buck <zpatrickbuck@gmail.com>
2026-07-31 13:10:39 +0400

    Restore SPARRC location notification title

--- lib/tsbg_engine.dart
+++ lib/tsbg_engine.dart
@@ -174,7 +174,7 @@ class TsbgEngine {
             // The notification still appears in the shade (OS requirement for
             // foreground services) but is otherwise invisible during normal use.
             notification: fbg.Notification(
-              title: '',
+              title: 'SPARRC',
               text: '',
               smallIcon: 'drawable/ic_stat_ic_launcher_foreground',
               priority: fbg.NotificationPriority.min,
